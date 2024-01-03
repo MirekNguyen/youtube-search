d83f3d894d4d865ae156575bc9f3e729354be9a8
Mirek Nguyen <mireknguyenbinh@seznam.cz>
2024-01-03 22:16:43 +0100

    feat: video info

--- video_details.py
+++ video_details.py
@@ -1,7 +1,11 @@
 """This module is used to search for the get the details of the videos."""
+from datetime import datetime
+
 import requests
+from isodate import parse_duration
 
 VIDEO_DETAILS_URL = "https://www.googleapis.com/youtube/v3/videos?"
+
 
 def video_details(video_ids, api_key):
     """This function is used to get the details of the videos."""
@@ -14,9 +18,39 @@ def video_details(video_ids, api_key):
     videos = video_response.json()
     return videos
 
+
+def video_info(videos):
+    """This function is used to get the details of the videos."""
+    video_items = videos.get("items", [])
+    video_objects = list(
+        map(
+            lambda video: {
+                "title": video["snippet"]["title"],
+                "duration": parse_duration(
+                    video["contentDetails"]["duration"]
+                ).total_seconds(),
+                "is_short": is_short(
+                    parse_duration(video["contentDetails"]["duration"]).total_seconds()
+                ),
+                "published_at": datetime.strptime(
+                    video["snippet"]["publishedAt"], "%Y-%m-%dT%H:%M:%SZ"
+                ),
+            },
+            video_items,
+        )
+    )
+    return video_objects
+
+
+def is_short(duration):
+    """This function is used to check if the video is short."""
+    return duration <= 60
+
+
 def print_video_details(videos):
     """This function is used to print the details of the videos."""
-    for video in videos.get("items", []):
-        title = video["snippet"]["title"]
-        duration = video["contentDetails"]["duration"]
-        print(f"{title} - {duration}")
+    video_list = video_info(videos)
+    for video in video_list:
+        print(
+            f"{video['title']}-{video['duration']}-{video['is_short']}-{video['published_at']}"
+        )
