8d6bd28ee1d05ae5c37e62636378d3085d45adad
Mirek Nguyen <mireknguyenbinh@seznam.cz>
2024-01-03 19:28:46 +0100

    feat: argument parser

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ import os
 
 import requests
 from dotenv import load_dotenv
+import argparse
+
+parser = argparse.ArgumentParser(description="Youtube Search")
+parser.add_argument("-c", "--channel", action="store", help="Channel ID (required)")
+
+args = parser.parse_args()
+
+if not args.channel:
+    print("Please specify a channel ID")
+    exit(1)
 
 load_dotenv()
 api_key = os.getenv("YOUTUBE_DATA_API_KEY")
+
