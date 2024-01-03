de39692171fe5eb44bcc2a7220f0cecda2552c00
Mirek Nguyen <mireknguyenbinh@seznam.cz>
2024-01-03 20:03:30 +0100

    feat: pylint recommendations

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+"""Youtube Search"""
 import os
+import sys
 
+import argparse
 import requests
 from dotenv import load_dotenv
-import argparse
 
 parser = argparse.ArgumentParser(description="Youtube Search")
 parser.add_argument("-c", "--channel", action="store", help="Channel ID (required)")
@@ -11,8 +13,23 @@ args = parser.parse_args()
 
 if not args.channel:
     print("Please specify a channel ID")
-    exit(1)
+    sys.exit(1)
 
 load_dotenv()
 api_key = os.getenv("YOUTUBE_DATA_API_KEY")
+channel_id = args.channel
+BASE_VIDEO_URL = "https://www.googleapis.com/youtube/v3/search?"
 
+params = {
+    "part": "snippet",
+    "channelId": channel_id,
+    "maxResults": 1,
+    "order": "date",
+    "type": "video",
+    "key": api_key,
+}
+
+response = requests.get(BASE_VIDEO_URL, params=params, timeout=5)
+if response.status_code != 200:
+    print("Error: ", response.status_code)
+    sys.exit(1)
