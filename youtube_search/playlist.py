53f173e4097dce33b7b8435d99a9308dccf3f768
Mirek Nguyen <mireknguyenbinh@seznam.cz>
2024-01-15 22:21:24 +0100

    refactor: formatting

--- youtube_search/playlist.py
+++ youtube_search/playlist.py
@@ -12,23 +12,20 @@ def get_playlist_id(channel_id, api_key):
     params = {"part": "contentDetails", "id": channel_id, "key": api_key}
     response = requests.get(CHANNEL_URL, params=params, timeout=5)
     if response.status_code != 200:
-        error_message = response.json()["error"]['message']
+        error_message = response.json()["error"]["message"]
         status_code = response.status_code
         raise YoutubeAPIError(
             f"Error throwed by the API ({status_code}): {error_message}"
         )
     return response
 
+
 def get_url_info(channel_name, api_key):
-    params = {
-        'part': 'snippet',
-        'type': 'channel',
-        'q': channel_name,
-        'key': api_key
-    }
+    """Get channel info from channel name"""
+    params = {"part": "snippet", "type": "channel", "q": channel_name, "key": api_key}
     response = requests.get(SEARCH_URL, params, timeout=5)
     if response.status_code != 200:
-        error_message = response.json()["error"]['message']
+        error_message = response.json()["error"]["message"]
         status_code = response.status_code
         raise YoutubeAPIError(
             f"Error throwed by the API ({status_code}): {error_message}"
--- youtube_search/settings.py
+++ youtube_search/settings.py
@@ -2,25 +2,32 @@
 
 import argparse
 import os
+
 from dotenv import load_dotenv
 
 
 class Settings:
     """Settings for youtube_search"""
+
     def __init__(self) -> None:
         self.args = self.parse_args()
         self.env = self.load_env()
+
     def parse_args(self):
         """Parse arguments"""
         parser = argparse.ArgumentParser(description="Youtube Search")
+        parser.add_argument("-c", "--channel", action="store", help="Channel ID")
         parser.add_argument(
-            "-c", "--channel", action="store", help="Channel ID (required)"
-        )
-        parser.add_argument(
-            "-r", "--results", action="store", help="Number of results", default=1
+            "-r",
+            "--results",
+            action="store",
+            help="Number of results (default 1)",
+            default=1,
         )
         parser.add_argument("-o", "--output", action="store", help="Generate RSS feed")
-        parser.add_argument("-t", "--timezone", action="store", help="Timezone")
+        parser.add_argument(
+            "-t", "--timezone", action="store", help="Timezone (default Etc/UTC)"
+        )
         parser.add_argument("-p", "--playlist", action="store", help="Playlist ID")
         parser.add_argument("--get-channel", action="store", help="Get channel info")
         parser.add_argument(
